6adfc80ae8bbe5c8173278776bfba59b5375bd8f
minjaposao <minjaposao@gmail.com>
2022-03-10 11:17:55 +0100

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,27 @@
-import {API_URL} from '../config/index'
+import { API_URL } from '../config/index';
+import Link from 'next/link';
 
-export default function Home({events}) {
+export default function Home({ events }) {
   return (
     <div>
-     <h1>Sajt</h1>
-     {events.map(evt=>(
-       <h3 key={evt.id}>{evt.id}</h3>
-     ))}
+      <h1>Sajt</h1>
+      {events.map((evt) => (
+        <h3 key={evt.id}>{evt.id}</h3>
+      ))}
+      <Link href='/about'>About Page</Link>
     </div>
-  )
+  );
 }
-export async function getServerSideProps(){
-  const res = await fetch(`${API_URL}/api/events`)
-  const json = await res.json()
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const json = await res.json();
 
   const events = json.data;
   // console.log(events)
 
-  return{
-    props:{
-      events
-    }
-  }
+  return {
+    props: {
+      events,
+    },
+  };
 }
